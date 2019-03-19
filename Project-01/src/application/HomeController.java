bea07d2e40788b337cd9e826c53d9c46255c8956
range <range@10.16.61.28>
2019-03-18 21:05:34 -0400

    The Order button in the home page is linked to the Order Window

--- Project-01/src/application/HomeController.java
+++ Project-01/src/application/HomeController.java
@@ -1,5 +1,39 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class HomeController {
-	
+	@FXML private Button btnOrder;
+
+	@FXML private void initialize(){
+
+		btnOrder.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event){
+				onOrderClicked();
+			}
+		});
+
+
+	}
+
+	public void onOrderClicked(){
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Order.fxml"));
+			Scene scene = new Scene(root,700,860);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+
+	}
+	}
 }
