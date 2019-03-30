abcc3f15df996a7ec0cfadb39efa05622c365d72
range <range@10.48.41.191>
2019-03-29 22:32:31 -0400

    In the HomeController, firstname and lastname variables are created and then the values of textfield are assigned to them using getText in name(). the following variables are passed to the OrderController class by creating an object of OrderController class "display" and the values are passed as parameters of the username() method in the OrderController class.

--- Project-01/src/application/HomeController.java
+++ Project-01/src/application/HomeController.java
@@ -2,8 +2,11 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
@@ -12,6 +15,17 @@ import javafx.event.EventHandler;
 public class HomeController {
 	@FXML private Button btnOrder;
 	@FXML private Button btnReservation;
+	@FXML private TextField fieldFirstName;
+	@FXML private TextField fieldLastName;
+
+	//private UserName userName;
+	//@FXML OrderController orderController;
+
+	 String firstName;
+	 String lastName;
+
+	ImageView img = new ImageView();
+
 
 	@FXML private void initialize(){
 
@@ -27,36 +41,53 @@ public class HomeController {
 			}
 		});
 
-
+	}
+	public void name(){
+		firstName = fieldFirstName.getText();
+		lastName = fieldLastName.getText();
 	}
 
 	public void onOrderClicked(){
+		name();
+
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("Order.fxml"));
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Order.fxml"));
-			Scene scene = new Scene(root,700,860);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Stage stage = new Stage();
-			stage.setScene(scene);
-			stage.show();
+			Loader.load();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 
 		}
+		OrderController display = Loader.getController();
+		display.name(firstName, lastName);
+		Parent root = Loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.showAndWait();
+
 	}
 
 
 	public void onReservationClicked(){
+		name();
+
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("Reservation.fxml"));
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Reservation.fxml"));
-			Scene scene = new Scene(root,700,860);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Stage stage = new Stage();
-			stage.setScene(scene);
-			stage.show();
+			Loader.load();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 
 		}
+		OrderController display = Loader.getController();
+		display.name(firstName, lastName);
+		Parent root = Loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.showAndWait();
+
 	}
 
 
--- Project-01/src/application/OrderController.java
+++ Project-01/src/application/OrderController.java
@@ -7,6 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,7 +18,21 @@ public class OrderController {
 	@FXML private Button btnReservation;
 	@FXML private Button btnConfirm;
 	@FXML private Button btnCancel;
+	@FXML private TextField username;
+	@FXML private ImageView imageView1;
+	@FXML private Label usernameL;
 
+	@FXML private Button lMeals;
+	@FXML private Button rtMeals;
+	@FXML private Button lBeverages;
+	@FXML private Button rBeverages;
+	@FXML private Button lDesserts;
+	@FXML private Button rDesserts;
+
+	//private UserName userName;
+
+	//String[] _images = {"1.jpg", "2.jpg", "3.jpg"};
+   Images images = new Images();
 
 	@FXML public void initializer(){
 
@@ -38,7 +55,36 @@ public class OrderController {
 				onCancelClicked();
 			}
 		});
+
+
+		lMeals.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				onLeftMealsClicked();
+			}
+		});
+
+
+
 	}
+
+	public void name(String first, String last){
+		String display = first + " " + last;
+		username.setText(display);
+	}
+
+
+
+	public void onLeftMealsClicked(){
+		int index = 0;
+		images.setimage(index);
+		String imageURL = images.getimage();
+    	Image image = new Image(imageURL);
+    	imageView1.setImage(image);
+    	index++;
+
+	}
+
+
 
 
 	public void onReservationClicked(){
